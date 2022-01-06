1da44ea77b815f23ded1fc1a77e808d84c9392db
Star <91126802+MoonByoll@users.noreply.github.com>
2022-01-06 15:02:31 +0330

    star had changed the main file
    please rename the main into Source

--- main.cpp
+++ main.cpp
@@ -1,32 +1,66 @@
 #include <iostream>
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+#include"MainMenu.h"
+
+using namespace std;
 
 int main()
 {
-    sf::ContextSettings settings;
-    settings.antialiasingLevel = 8;
-    sf::RenderWindow window(sf::VideoMode(800, 600), "My window", sf::Style::Default, settings);
+	sf::ContextSettings settings;
+	settings.antialiasingLevel = 8;
+	sf::RenderWindow window(sf::VideoMode(600, 600), "My window", sf::Style::Default, settings);
 
-    sf::CircleShape circle(200.f, 100);
-    circle.setFillColor(sf::Color(0, 170, 170));
+	//we make it more dynamic in here instead of giving it the amount of 800 and 600 our selves
+	MainMenu menuDisplay(window.getSize().x, window.getSize().y);
 
-    // run the program as long as the window is open
-    while (window.isOpen())
-    {
-        // check all the window's events that were triggered since the last iteration of the loop
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            // "close requested" event: we close the window
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+	// run the program as long as the window is open
+	while (window.isOpen())
+	{
+		// check all the window's events that were triggered since the last iteration of the loop
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			switch (event.type)
+			{
+			case sf::Event::KeyReleased:
+				switch (event.key.code)
+				{
+				case sf::Keyboard::Up:
+					menuDisplay.moveUp();
+					break;
 
-        window.clear();
-        window.draw(circle);
-        window.display();
-    }
+				case sf::Keyboard::Down:
+					menuDisplay.moveDown();
+					break;
 
-    return 0;
+				case sf::Keyboard::Return:
+					switch (menuDisplay.getPressedItem())
+					{
+					case 0:
+						cout << "PLAY is pressed" << endl;
+						break;
+
+					case 1:
+						cout << "Game setting is pressed" << endl;
+						break;
+
+					case 2:
+						cout << "exit  is pressed" << endl;
+						break;
+					}
+				}
+				break;
+			case sf::Event::Closed:
+				window.close();
+				break;
+			}
+		}
+
+		window.clear(sf::Color::Magenta);
+		menuDisplay.draw(window);
+		window.display();
+	}
+
+	return 0;
 }
