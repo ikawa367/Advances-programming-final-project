827b354d68610ecd2dc95df461aaa48f23b80397
moonbyoll2002@gmail.com <moonbyoll2002@gmail.com>
2022-01-21 02:51:35 +0330

    Star has fixed bugs of random fruit and snakes

--- test/SnakeFunc.h
+++ test/SnakeFunc.h
@@ -5,98 +5,92 @@
 #include <deque>
 #include <thread>
 
-class Apple{
+class Apple {
     sf::RectangleShape sprite;
     sf::RectangleShape sprite2;
 public:
-    Apple()
-    {
-        sf::Vector2f startPosition1(400,400);
-        sf::Vector2f startPosition2(450,300);
-        sprite.setSize(sf::Vector2f(20,20));
-        sprite2.setSize(sf::Vector2f(20,20));
+    Apple() {
+        sf::Vector2f startPosition1(800, 800);
+        sf::Vector2f startPosition2(800, 800);
+        sprite.setSize(sf::Vector2f(20, 20));
+        sprite2.setSize(sf::Vector2f(20, 20));
         sprite.setFillColor(sf::Color::Red);
         sprite2.setFillColor(sf::Color::Blue);
         sprite.setPosition(startPosition1);
         sprite2.setPosition(startPosition2);
     }
 
-    void setPosition(sf::Vector2f newPosition1 , sf::Vector2f newPosition2)
-    {
+    void setPosition(sf::Vector2f newPosition1, sf::Vector2f newPosition2) {
         sprite.setPosition(newPosition1);
         sprite2.setPosition(newPosition2);
     }
 
     //draw function needs getSprite.
-    sf::RectangleShape getSprite1()
-    {
+    sf::RectangleShape getSprite1() {
         return sprite;
     }
-    sf::RectangleShape getSprite2()
-    {
+
+    sf::RectangleShape getSprite2() {
         return sprite2;
     }
 };
 
 
-class snakeSection{
+class snakeSection {
     sf::Vector2f position1;
     sf::Vector2f position2;
     sf::RectangleShape section1;
     sf::RectangleShape section2;
 public:
-    snakeSection(sf::Vector2f startPosition1)
-    {
+    snakeSection(sf::Vector2f startPosition1) {
         sf::Vector2f startPosition2;
-        startPosition2.x=startPosition1.x;
-        startPosition2.y=startPosition1.y;
-        section1.setSize(sf::Vector2f(20,20));
-        section2.setSize(sf::Vector2f(20,20));
+        startPosition2.x = startPosition1.x;
+        startPosition2.y = startPosition1.y;
+        section1.setSize(sf::Vector2f(20, 20));
+        section2.setSize(sf::Vector2f(20, 20));
         section1.setFillColor(sf::Color::Green);
         section2.setFillColor(sf::Color::Yellow);
         section1.setPosition(startPosition1);
-        section2.setPosition(startPosition1.x+50,startPosition1.y+50);
-        position1=startPosition1;
-        position2=startPosition2;
+        section2.setPosition(startPosition1.x, startPosition1.y);
+        position1 = startPosition1;
+        position2 = startPosition2;
     }
 
-    sf::Vector2f getPosition1()
-    {
+    sf::Vector2f getPosition1() {
         return position1;
     }
-    sf::Vector2f getPosition2()
-    {
+
+    sf::Vector2f getPosition2() {
         return position2;
     }
-    void setPosition1(sf::Vector2f newPosition)
-    {
-        position1=newPosition;
+
+    void setPosition1(sf::Vector2f newPosition) {
+        position1 = newPosition;
     }
-    void setPosition2(sf::Vector2f newPosition)
-    {
-        position2=newPosition;
+
+    void setPosition2(sf::Vector2f newPosition) {
+        position2 = newPosition;
     }
-    sf::RectangleShape getShape1()
-    {
+
+    sf::RectangleShape getShape1() {
         return section1;
     }
-    sf::RectangleShape getShape2()
-    {
+
+    sf::RectangleShape getShape2() {
         return section2;
     }
-    void update()
-    {
+
+    void update() {
         section1.setPosition(position1);
         section2.setPosition(position2); //TODO: dangerous
     }
 };
 
-class Engine
-{
+class Engine {
     //sf::Vector2f resolution; be dard nemikhore
     sf::RenderWindow snakeWindow;
-    vector<snakeSection>snake;
-    vector<snakeSection>snake2;
+    vector<snakeSection> snake;
+    vector<snakeSection> snake2;
     int snake1Direction;
     int snake2Direction;
     deque<int> directionQueue1;
@@ -112,21 +106,17 @@ public:
 
     Engine() {
         snake1Direction = direction::RIGHT;
-        snake2Direction = direction::RIGHT;
+        snake2Direction = direction::LEFT;
         timeSinceLastMove = sf::Time::Zero;
         newSnake();
         moveApple();
-        sectionsToAdd=0;
+        sectionsToAdd = 0;
     }
 
-    void setSpeed(bool hard)
-    {
-        if(hard == true)
-        {
+    void setSpeed(bool hard) {
+        if (hard == true) {
             speed = 4;
-        }
-        else
-        {
+        } else {
             speed = 2;
         }
     }
@@ -279,7 +269,7 @@ public:
                 case direction::RIGHT:
                     if (thisSectionPosition2.x > 800) {
                         thisSectionPosition2.x = -20;
-                        snake2[0].setPosition2(sf::Vector2f(thisSectionPosition2.x, thisSectionPosition2.y));
+                        snake2[0].setPosition2(sf::Vector2f(thisSectionPosition2.x + 20, thisSectionPosition2.y));
                     }
                     snake2[0].setPosition2(sf::Vector2f(thisSectionPosition2.x + 20, thisSectionPosition2.y));
                     break;
@@ -335,24 +325,21 @@ public:
         }
     }
 
-    void draw()
-    {
+    void draw() {
         snakeWindow.clear(sf::Color::Black);
         snakeWindow.draw(apple.getSprite1());
         snakeWindow.draw(apple.getSprite2());
-        for (auto &s: snake)
-        {
-            for (auto &s: snake)
-            {
+        for (auto &s: snake) {
+            for (auto &s: snake) {
                 snakeWindow.draw(s.getShape1());
             }
-            for (auto &s: snake2)
-            {
+            for (auto &s: snake2) {
                 snakeWindow.draw(s.getShape2());
             }
             snakeWindow.display();
         }
     }
+
     void run() {
         sf::Clock clock;
         snakeWindow.create(sf::VideoMode(800, 600), "main game");
@@ -367,43 +354,43 @@ public:
         }
     }
 
-    void newSnake(/*sf:: Vector2f start*/) {
+    void newSnake() {
         snake.clear();
         snake2.clear();
         snake.push_back(sf::Vector2f(100, 100)); //TODO: khodesh az emplace_pack estefade karde.
         snake.push_back(sf::Vector2f(80, 100));
         snake.push_back(sf::Vector2f(60, 100));
-        snake2.push_back(sf::Vector2f(100, 150));
-        snake2.push_back(sf::Vector2f(80, 150));
-        snake2.push_back(sf::Vector2f(60, 150));
+        snake2.push_back(sf::Vector2f(700, 100));
+        snake2.push_back(sf::Vector2f(720, 100));
+        snake2.push_back(sf::Vector2f(740, 100));
     }
 
     void addSnakeSection() {
         sf::Vector2f newSectionPosition = snake[snake.size() - 1].getPosition1(); //-1 because index starts with 0
         snake.push_back(newSectionPosition);
     }
-    void moveApple()
-    {
+
+    void moveApple() {
         sf::Vector2f appleLocation1;
         sf::Vector2f appleLocation2;
-        bool badLocation= false;
+        bool badLocation = false;
         srand(time(nullptr));
-        do{
-            badLocation= false;
-            appleLocation1.x=(rand() % 116)*(rand() % 4);
-            appleLocation2.x=appleLocation1.x + 20*(rand() % 4);
-            appleLocation1.y=rand() % 100 * (rand()%4);
-            appleLocation2.y=appleLocation1.y + 20*(rand()%4);
-            for(auto &i: snake)
-            {
-                if(i.getShape1().getGlobalBounds().intersects(sf::Rect<float>(appleLocation1.x,appleLocation1.y,20,20))&&
-                        i.getShape1().getGlobalBounds().intersects(sf::Rect<float>(appleLocation2.x,appleLocation2.y,20,20)))
-                {
-                    badLocation= true;
+        do {
+            badLocation = false;
+            appleLocation1.x =  (rand()%40)*20;
+            appleLocation2.x =  (rand()%40)*20;
+            appleLocation1.y =  (rand()%30)*20;
+            appleLocation2.y =  (rand()%30)*20;
+            for (auto &i: snake) {
+                if (i.getShape1().getGlobalBounds().intersects(
+                        sf::Rect<float>(appleLocation1.x, appleLocation1.y, 20, 20)) &&
+                    i.getShape1().getGlobalBounds().intersects(
+                            sf::Rect<float>(appleLocation2.x, appleLocation2.y, 20, 20))) {
+                    badLocation = true;
                     break;
                 }
             }
         } while (badLocation);
-        apple.setPosition(appleLocation1,appleLocation2);
+        apple.setPosition(appleLocation1, appleLocation2);
     }
 };
